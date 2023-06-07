9662708ace859b6a413bc97a026c575ab51091dd
Harriet Oteng <harriet.oteng@gmail.com>
2023-06-07 14:22:52 +0000

    Add queries to create 'owners' and 'species' tables

--- schema.sql
+++ schema.sql
@@ -11,3 +11,23 @@ CREATE TABLE animals (
 
 -- Add species column to already created animals table
 ALTER TABLE animals ADD species VARCHAR(50);
+
+CREATE TABLE owners (
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  full_name VARCHAR(50) NOT NULL,
+  age INT NOT NULL
+);
+
+CREATE TABLE species (
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  name VARCHAR(50) NOT NULL
+);
+
+ALTER TABLE student_details  
+DROP COLUMN student_age;
+
+ALTER TABLE child_table
+ADD CONSTRAINT constraint_fk
+FOREIGN KEY (fk_columns)
+REFERENCES parent_table(parent_key_columns)
+ON DELETE CASCADE;
