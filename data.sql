a9bd3bee31f5b7af174f24142d12cf84fa0c62c7
Harriet Oteng <harriet.oteng@gmail.com>
2023-06-07 15:35:08 +0000

    Add sample data to owners and species table

--- data.sql
+++ data.sql
@@ -18,3 +18,19 @@ VALUES
   ('Blossom', '1998-10-13', 3, '1', 17),
   ('Ditto', '2022-05-14', 4, '1', 22)
 ;
+
+INSERT INTO owners (full_name, age)
+VALUES
+  ('Sam Smith', 34),
+  ('Jennifer Orwell', 19),
+  ('Bob', 45),
+  ('Melody Pond', 77),
+  ('Dean Winchester', 14),
+  ('Jodie Whittaker', 38)
+;
+
+INSERT INTO species (name)
+VALUES
+  ('Pokemon'),
+  ('Digimon')
+;
