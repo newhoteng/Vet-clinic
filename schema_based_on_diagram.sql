8cd46b0f5f93ffca3e4b9993c1730ad41c247d39
Joy Phoebe <joyapisi@gmail.com>
2023-06-14 11:20:02 +0300

    Add 3 tables according to given diagram and one join table

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -21,3 +21,24 @@ CREATE TABLE invoices (
   payed_at TIMESTAMP NOT NULL,
   medical_history_id INT REFERENCES medical_histories(id) ON UPDATE CASCADE ON DELETE CASCADE
 );
+
+CREATE TABLE invoices(
+  id SERIAL PRIMARY KEY NOT NULL,
+	total_amount decimal,
+	generated_at timestamp,
+	payed_at timestamp,
+	medical_history_id int,
+  FOREIGN KEY (medical_history_id) REFERENCES medical_histories(id)
+);
+
+CREATE TABLE treatments (
+  id SERIAL PRIMARY KEY NOT NULL,
+	type varchar(255),
+	name  varchar(255)
+);
+
+CREATE TABLE medical_histories_treatment (
+  id SERIAL PRIMARY KEY NOT NULL, 
+  medical_histories_id int REFERENCES medical_histories(id),
+  treatment_id int REFERENCES treatment(id)
+);
